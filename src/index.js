60f643c4bdab476c3b67cd7a5c9ce71be4a94c70
MaksGlushko <133649841+MaksGlushko@users.noreply.github.com>
2024-01-27 12:57:24 +0100

    react upd

--- src/index.js
+++ src/index.js
@@ -7,15 +7,14 @@ import { persistor, store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/react-homework-template">
-        <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
-        </PersistGate>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </>
-);
+  </React.StrictMode>
+)
